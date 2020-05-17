e25ab8571134f909824bae4edfc3ee0ffd3669fe
dey <jagothjyotidey91@gmail.com>
2020-05-17 04:37:41 +0200

    Initial Commit

---
+++ ARKit-3DModel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARKit-3DModel
+//
+//  Created by Jagoth Jyoti Dey on 17.05.20.
+//  Copyright © 2020 Jagoth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
